4a3053df9ca81452771b88a77ba81d9b4d98ff6e
Madwicked <spam4apb@gmail.com>
2022-03-12 21:11:30 +0300

    1

--- 3.2.2.py
+++ 3.2.2.py
@@ -2,10 +2,18 @@ s = input().lower().split()
 print(s)
 d={}
 for i in s:
-   if i in d:
-       d[i]+=1
-   else:
-       d[i]=1
+    if i in d:
+        d[i]+=1
+    else:
+        d[i]=1
 for i in d:
     print(i, d[i])
 #s = set()
+
+
+
+
+
+
+
+
---
+++ 3.2.3.py
@@ -0,0 +1,6 @@
+n = int(input())
+k=0
+while k<=n:
+    x=int(input())
+    k+=1
+    print(x)
